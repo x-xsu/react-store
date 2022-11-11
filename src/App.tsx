9550d3199b6fc45bbf8c3d60a4cfe84555244a12
Ksenia Vasileva <k.vasilyeva@mediasoft.team>
2022-11-11 14:15:14 +0400

    init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Product } from "./components/Product";
+
+function App() {
+  return (
+    <div className='container'>
+      <Product />
+    </div>
+  );
+}
+
+export default App;
