62bb552f8748a273db96f20efd1cb659c03b2a67
Ksenia Vasileva <k.vasilyeva@mediasoft.team>
2022-11-21 18:37:50 +0400

    categories output

--- src/components/ProductsCategory/index.tsx
+++ src/components/ProductsCategory/index.tsx
@@ -1,8 +1,33 @@
 import styles from "./styles.module.scss";
+import axios from "../../axios";
+import { useEffect, useState } from "react";
+
+import { ReactComponent as CloseSvg } from "../../assets/svg/close.svg"
 
 export function ProductsCategory() {
+  const [category, setCategory] = useState([])
+
+  async function productsCategory() {
+    const res = await axios.get("products/categories").then((res) => {
+      return res.data
+    })
+    setCategory(res)
+  }
+
+  useEffect(() => {
+    productsCategory()
+  })
+
   return (
     <div className={ styles.category }>
+      {
+        category.map((category, index: number) => (
+          <button className={ "btn btnWhite" } key={ index }>
+            <CloseSvg />
+            <span>{ category }</span>
+          </button>
+        ))
+      }
 
     </div>
   )
